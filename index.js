cee75b41fa90470335aac50d01eb9ea548525548
Tiwari <mayand.tiwari@capgemini.com>
2018-05-23 07:30:21 -0600

    updated mongo creds

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const config = require('config');
 const express = require('express');
 const mongoose = require('mongoose');
 const pets = require('./routes/pets')
@@ -6,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use('/pets', pets);
 
-mongoose.connect('mongodb://localhost/pets')
+mongoose.connect(config.get('db'))
     .then(()=> console.log('connected to MongoDB..'))
     .catch(err=> console.error('couldnt connect to mongodb',err));
 
