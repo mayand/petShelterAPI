fc98f56b02714c94a031dee98d4b5e9f0176bc4e
Tiwari <mayand.tiwari@capgemini.com>
2018-05-23 08:39:59 -0600

    CORS changes

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 const config = require('config');
 const express = require('express');
 const mongoose = require('mongoose');
