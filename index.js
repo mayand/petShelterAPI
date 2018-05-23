a013da68296841f7209ec12d5c6ce9ccf15c5038
Tiwari <mayand.tiwari@capgemini.com>
2018-05-23 08:42:57 -0600

    CORS headers

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
 const config = require('config');
 const express = require('express');
 const mongoose = require('mongoose');
@@ -10,6 +5,11 @@ const pets = require('./routes/pets')
 
 const app = express();
 app.use(express.json());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 app.use('/pets', pets);
 
 mongoose.connect(config.get('db'))
